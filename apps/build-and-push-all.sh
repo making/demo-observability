61b6815e501606c66d6b3de0cc76325078e86108
Toshiaki Maki <tmaki@pivotal.io>
2019-07-03 13:35:41 +0900

    Use Greenwich.SR2

--- apps/build-and-push-all.sh
+++ apps/build-and-push-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# for f in $(find . -name pom.xml);do
-#  pushd $(dirname $f);
-#  ./mvnw clean package -DskipTests=true
-#  popd
-# done
+for f in $(find . -name pom.xml);do
+ pushd $(dirname $f);
+ ./mvnw clean package -DskipTests=true
+ popd
+done
 
 for f in $(find . -name manifest.yml);do
  cf push -f $f;
